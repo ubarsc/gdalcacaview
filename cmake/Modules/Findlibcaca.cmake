a6681fc0e44eb9ddbfb66ea15111ab34b4a386d5
Sam Gillingham <gillingham.sam@gmail.com>
2019-02-15 13:05:03 +1000

    use find_package etc in cmake to find gdal and libcaca

---
+++ cmake/Modules/Findlibcaca.cmake
@@ -0,0 +1,14 @@
+# stolen from https://github.com/foxkit-us/supergameherm/blob/master/cmake/Modules/Findlibcaca.cmake
+include(CheckSymbolExists)
+
+find_path(libcaca_INCLUDE_DIR caca.h)
+find_library(libcaca_LIBRARY caca)
+check_symbol_exists(CACA_API_VERSION_1 caca.h CACA_API_VERSION_1)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(libcaca FOUND_VAR libcaca_FOUND
+	REQUIRED_VARS libcaca_LIBRARY libcaca_INCLUDE_DIR
+	VERSION_VAR CACA_API_VERSION_1)
+
+mark_as_advanced(libcaca_INCLUDE_DIR libcaca_LIBRARY)
